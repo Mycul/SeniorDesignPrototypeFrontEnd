07bd0eae96fccf2f2d9c1abba8c16fe43893f3e3
Mycul <enriquez_michael@verizon.net>
2018-03-29 19:35:16 -0700

    Camera sends picture to storage bucket and downloadable link to picture is generated and usable

--- app/src/main/java/com/example/michael/prototypev2/Camera.java
+++ app/src/main/java/com/example/michael/prototypev2/Camera.java
@@ -7,6 +7,7 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -18,6 +19,7 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageMetadata;
@@ -34,26 +36,66 @@ import java.util.UUID;
 
 public class Camera extends AppCompatActivity {
 
-    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int CAMERA_REQUEST_CODE = 1;
 
-    private FirebaseStorage storage = FirebaseStorage.getInstance();
+
     String mUsername;
     String mUserID;
 
+
     private ProgressBar mProgressBar;
     private Button mCameraButton;
-    private TextView downloadUrl;
-    private Uri mImageUri;
+    
+    String mCurrentPhotoPath;
+
+    private StorageReference storage;
+    private TextView downloadUrlTextView;
 
 
-    private Bitmap mImageBitmap;
-    private String mCurrentPhotoPath;
-    private ImageView mImageView;
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+    public void takePhoto(View view){
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File...
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                /*Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.michael.prototypev2.fileprovider",
+                        photoFile);
+                        */
+                //takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE);
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+
+        storage = FirebaseStorage.getInstance().getReference();
 
         Bundle extras = getIntent().getExtras();
         if(extras != null){
@@ -63,187 +105,50 @@ public class Camera extends AppCompatActivity {
 
         mProgressBar = (ProgressBar) findViewById(R.id.uploadPictureSpinner);
         mCameraButton = (Button) findViewById(R.id.camera_button);
+        downloadUrlTextView = (TextView) findViewById(R.id.donwloadUrlTextView);
     }
 
 
 
 
-    public void takePhoto(View view){
-        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (cameraIntent.resolveActivity(getPackageManager()) != null) {
-            // Create the File where the photo should go
-            File photoFile = null;
-            try {
-                photoFile = createImageFile();
-            } catch (IOException ex) {
-                // Error occurred while creating the File
-                //Log.i(TAG, "IOException");
-            }
-            // Continue only if the File was successfully created
-            if (photoFile != null) {
-                //mImageUri = Uri.fromFile(photoFile);
-                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, setImageUri(photoFile));
-                startActivityForResult(cameraIntent, REQUEST_IMAGE_CAPTURE);
 
-            }
-        }
-    }
+
 
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            //try {
-                Toast.makeText(this, "Entered on Activity result", Toast.LENGTH_LONG).show();
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_REQUEST_CODE && resultCode == RESULT_OK) {
 
-            BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+            Toast.makeText(this, "Entered on Activity result", Toast.LENGTH_LONG).show();
+            //Uri file = Uri.fromFile(photoFile);
+            Uri uri = data.getData();
 
-            Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-
-
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
-            //bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
-            byte[] cameraData = baos.toByteArray();
-
-            String path = "SnapNSnack/" + UUID.randomUUID() + ".jpg";
-            StorageReference firebaseStorageRef = storage.getReference(path);
-
-
-
-
-            StorageMetadata metadata = new StorageMetadata.Builder()
-                    .setCustomMetadata("userID", mUserID)
-                    .build();
             mProgressBar.setVisibility(View.VISIBLE);
             mCameraButton.setEnabled(false);
-            UploadTask uploadTask = firebaseStorageRef.putBytes(cameraData, metadata);
-            uploadTask.addOnSuccessListener(Camera.this, new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            StorageReference filepath = storage.child("Photos").child(uri.getLastPathSegment());
+            filepath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
                 @Override
                 public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    // taskSnapshot.getMetadata() contains file metadata such as size, content-type, and download URL.
                     mProgressBar.setVisibility(View.GONE);
                     mCameraButton.setEnabled(true);
 
-                    //Uri url = taskSnapshot.getDownloadUrl();
-                    //downloadUrl.setText(url.toString());
-                   // downloadUrl.setVisibility(View.VISIBLE);
+
+                    Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                    String stringDL = downloadUrl.toString();
+                    downloadUrlTextView.setText(downloadUrl.toString());
+                    downloadUrlTextView.setVisibility(View.VISIBLE);
                 }
             });
 
-                /*mImageBitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), Uri.parse(mCurrentPhotoPath));
-                //mImageView.setImageBitmap(mImageBitmap);
-                Bundle extras = data.getExtras();
 
-                Bitmap intentBitmap = (Bitmap) extras.get("data");
-
-                ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                intentBitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
-                byte[] cameraData = baos.toByteArray();
-
-
-
-                //test storage stuff
-                String path = "SnapNSnack/" + UUID.randomUUID() + ".png";
-                StorageReference firebaseStorageRef = storage.getReference(path);
-
-                StorageMetadata metadata = new StorageMetadata.Builder()
-                        .setCustomMetadata("userID", mUserID)
-                        .build();
-
-                mProgressBar.setVisibility(View.VISIBLE);
-                mCameraButton.setEnabled(false);
-                UploadTask uploadTask = firebaseStorageRef.putBytes(cameraData, metadata);
-                uploadTask.addOnSuccessListener(Camera.this, new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                    @Override
-                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                        mProgressBar.setVisibility(View.GONE);
-                        mCameraButton.setEnabled(true);
-
-                        Uri url = taskSnapshot.getDownloadUrl();
-                        downloadUrl.setText(url.toString());
-                        downloadUrl.setVisibility(View.VISIBLE);
-                    }
-                });
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            */
         }
     }
 
 
-    private File createImageFile() throws IOException{
-        //create and image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName, /* prefix */
-                ".jpg", /* suffix */
-                storageDir    /* directory */
-        );
-        //save a file: path for use with ACTION_VIEW intents
-        mCurrentPhotoPath = image.getAbsolutePath();
-        return image;
-    }
-    public Uri setImageUri(File photoFile){
-        Uri imgUri = Uri.fromFile(photoFile);
-        return imgUri;
-
-    }
-/*
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data){
-        super.onActivityResult(requestCode,resultCode,data);
-        switch(requestCode){
-            case TAKE_PICTURE:
-                if(resultCode == Activity.RESULT_OK){
-                    Uri selectedImage = imageUri;
-                    getContentResolver().notifyChange(selectedImage, null);
-                    Image
-                }
-        }
-    }
-*/
-/*
-    private void dispatchTakePictureIntent(){
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        //Ensure that there is a camera activity to handle the intent
-        if(takePictureIntent.resolveActivity(getPackageManager()) != null){
-            //create the file where the photo should go
-            File photoFile = null;
-            try{
-                photoFile = createImageFile();
-
-            }catch (IOException ex){
-                //error occured
-            }
-            //should only execute after the file was successfully created
-            if(photoFile != null){
-                Uri photoURI = FileProvider.getUriForFile(this,"com.example.android.michael.prototypev2.fileprovider", photoFile);
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
-            }
-        }
-    }
 
 
-     @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        if (mImageUri != null) {
-            outState.putString("cameraImageUri", mImageUri.toString());
-        }
-    }
-    @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-        if (savedInstanceState.containsKey("cameraImageUri")) {
-            mImageUri = Uri.parse(savedInstanceState.getString("cameraImageUri"));
-        }
-    }
-*/
 
 
 }
