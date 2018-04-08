8e9b95ce5fb425a2dd723bedbcf2b008c4b9ce03
Mycul <enriquez_michael@verizon.net>
2018-04-07 18:43:21 -0700

    Update Camera.java

--- app/src/main/java/com/example/michael/prototypev2/Camera.java
+++ app/src/main/java/com/example/michael/prototypev2/Camera.java
@@ -1,10 +1,14 @@
 package com.example.michael.prototypev2;
 
 import android.app.Activity;
+import android.content.ClipData;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
@@ -32,23 +36,28 @@ import java.io.IOException;
 import java.net.URI;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 public class Camera extends AppCompatActivity {
 
-    static final int CAMERA_REQUEST_CODE = 1;
+    static final int REQUEST_TAKE_PHOTO = 1;
 
 
     String mUsername;
     String mUserID;
 
-
+    private FirebaseStorage storage = FirebaseStorage.getInstance();
+    
+    private String pictureImagePath = "";
     private ProgressBar mProgressBar;
     private Button mCameraButton;
+    
+    File photoFile;
 
     String mCurrentPhotoPath;
 
-    private StorageReference storage;
+    //private StorageReference storage;
     private TextView downloadUrlTextView;
 
 
@@ -85,6 +94,35 @@ public class Camera extends AppCompatActivity {
                         photoFile);
                         */
                 //takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                Uri photoURI = FileProvider.getUriForFile(this,"com.example.android.michael.prototypev2.fileprovider", photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                
+                if (Build.VERSION.SDK_INT>= Build.VERSION_CODES.LOLLIPOP) {	
+                    takePictureIntent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);	
+                }	
+                else if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.JELLY_BEAN) {	
+                    ClipData clip=	
+                            ClipData.newUri(getContentResolver(), "A photo", photoURI);	
+	
+                    takePictureIntent.setClipData(clip);	
+                    takePictureIntent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);	
+                }	
+                else {	
+                    List<ResolveInfo> resInfoList=	
+                            getPackageManager()	
+                                    .queryIntentActivities(takePictureIntent, PackageManager.MATCH_DEFAULT_ONLY);	
+	
+                    for (ResolveInfo resolveInfo : resInfoList) {	
+                        String packageName = resolveInfo.activityInfo.packageName;	
+                        grantUriPermission(packageName, photoURI,	
+                                Intent.FLAG_GRANT_WRITE_URI_PERMISSION);	
+                    }	
+                }
+                
+                
+                
+                
+                
                 startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE);
             }
         }
@@ -95,7 +133,7 @@ public class Camera extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
 
-        storage = FirebaseStorage.getInstance().getReference();
+        //storage = FirebaseStorage.getInstance().getReference();
 
         Bundle extras = getIntent().getExtras();
         if(extras != null){
@@ -118,9 +156,43 @@ public class Camera extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == CAMERA_REQUEST_CODE && resultCode == RESULT_OK) {
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
 
             Toast.makeText(this, "Entered on Activity result", Toast.LENGTH_LONG).show();
+            File imgFile = new File(mCurrentPhotoPath);	
+	
+            Bitmap bitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());	
+	
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();	
+	
+	
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);	
+            byte[] cameraData = baos.toByteArray();	
+	
+            String path = "SnapNSnack/" + UUID.randomUUID() + ".jpg";	
+            StorageReference firebaseStorageRef = storage.getReference(path);	
+	
+	
+            StorageMetadata metadata = new StorageMetadata.Builder()	
+                    .setCustomMetadata("userID", mUserID)	
+                    .build();	
+            mProgressBar.setVisibility(View.VISIBLE);	
+            mCameraButton.setEnabled(false);	
+            UploadTask uploadTask = firebaseStorageRef.putBytes(cameraData, metadata);	
+            uploadTask.addOnSuccessListener(Camera.this, new OnSuccessListener<UploadTask.TaskSnapshot>() {	
+                @Override	
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {	
+                    mProgressBar.setVisibility(View.GONE);	
+                    mCameraButton.setEnabled(true);	
+	
+                    
+                    Uri downloadUrl = taskSnapshot.getDownloadUrl();	
+                    downloadUrlTextView.setText(downloadUrl.toString());	
+                    downloadUrlTextView.setVisibility(View.VISIBLE);	
+                }	
+            });
+            
+            /*
             //Uri file = Uri.fromFile(photoFile);
             Uri uri = data.getData();
 
@@ -141,12 +213,26 @@ public class Camera extends AppCompatActivity {
                     downloadUrlTextView.setVisibility(View.VISIBLE);
                 }
             });
-
+        */
 
         }
     }
 
-
+    @Override	
+    protected void onSaveInstanceState(Bundle outState) {	
+        super.onSaveInstanceState(outState);	
+        if (mCurrentPhotoPath != null) {	
+            outState.putString("mCurrentPhotoPath", mCurrentPhotoPath);	
+        }	
+    }	
+	 
+    @Override	
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {	
+        super.onRestoreInstanceState(savedInstanceState);	
+        if (savedInstanceState.containsKey("mCurrentPhotoPath")) {	
+            mCurrentPhotoPath = savedInstanceState.getString("mCurrentPhotoPath");	
+        }	
+    }
 
 
 
